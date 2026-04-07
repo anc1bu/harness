ef3ecc67a15ae4dc29ea68828685d7f52b1ae80d
Anil Cengiz <anlcngz81@gmail.com>
2026-04-07 18:58:37 +0200

    Run init_db in provision.sh to ensure schema on deploy

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -6,6 +6,9 @@ APPDIR=/var/www/harness
 # Install/update Python dependencies
 "$APPDIR/venv/bin/pip" install -q -r "$APPDIR/requirements.txt"
 
+# Ensure DB exists and schema is up to date
+"$APPDIR/venv/bin/python3" -c "from server import init_db; init_db()"
+
 # Restart service to pick up new code
 systemctl restart harness
 
